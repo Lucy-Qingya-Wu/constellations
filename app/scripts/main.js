6a97e4c623ea73ee2b9e02e5a16c06c4956c8d5a
Felise Uy <feliseuy@ca.ibm.com>
2015-07-23 17:10:27 -0400

    'tweaks'

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -50,7 +50,139 @@ function drawStar(cx, cy, spikes, outerRadius, innerRadius, color) {
 }
 
 
-var constellation = ;
+var constellation = {                     "name" : "Draco - dragon",
+                      "stars":[
+                      {
+                      "x_coordinate":32,
+                      "y_coordinate":50,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":10,
+                      "outerRadius":20 },
+
+                      {
+                      "x_coordinate":136,
+                      "y_coordinate":120,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":10,
+                      "outerRadius":20 },
+
+                      {
+                      "x_coordinate":140,
+                      "y_coordinate":20,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":10,
+                      "outerRadius":20 },
+
+                      {
+                      "x_coordinate":240,
+                      "y_coordinate":50,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":10,
+                      "outerRadius":20 },
+
+                      {
+                      "x_coordinate":136,
+                      "y_coordinate":450,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":10,
+                      "outerRadius":20 },
+
+                      {
+                      "x_coordinate":150,
+                      "y_coordinate":570,
+                      "color":"yellow",
+                      "numPoints":6,
+                      "innerRadius":10,
+                      "outerRadius":20 },
+
+                      {
+                      "x_coordinate":300,
+                      "y_coordinate":520,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":10,
+                      "outerRadius":20 },
+
+                      {
+                      "x_coordinate":400,
+                      "y_coordinate":400,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":10,
+                      "outerRadius":20 },
+
+                      {
+                      "x_coordinate":500,
+                      "y_coordinate":350,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":10,
+                      "outerRadius":20 },
+
+                      {
+                      "x_coordinate":600,
+                      "y_coordinate":300,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":10,
+                      "outerRadius":20 },
+
+                      {
+                      "x_coordinate":700,
+                      "y_coordinate":250,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":10,
+                      "outerRadius":20 },
+
+                      {
+                      "x_coordinate":800,
+                      "y_coordinate":200,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":10,
+                      "outerRadius":20 },
+
+                      {
+                      "x_coordinate":900,
+                      "y_coordinate":250,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":10,
+                      "outerRadius":20 },
+
+                      {
+                      "x_coordinate":1000,
+                      "y_coordinate":300,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":10,
+                      "outerRadius":20 },
+
+                      {
+                      "x_coordinate":1100,
+                      "y_coordinate":400,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":10,
+                      "outerRadius":20 },
+
+                      {
+                      "x_coordinate":1200,
+                      "y_coordinate":500,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":10,
+                      "outerRadius":20 }
+                      ]
+
+                }
+;
 
 document.getElementById("name").innerHTML = constellation["name"];
 
