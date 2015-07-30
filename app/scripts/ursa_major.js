55dd18fdd860e3d1fc161a02850c07ced728f995
Lucy-Qingya-Wu <lqy.wu@mail.utoronto.ca>
2015-07-30 15:44:46 -0400

    push ursa_major.js and scorpius.js into repository

---
+++ app/scripts/ursa_major.js
@@ -0,0 +1,147 @@
+stars = {            "name" : "Pegasus - winged horse",
+                      "stars":[
+                      {
+                      "x_coordinate":2,
+                      "y_coordinate":39,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":5,
+                      "outerRadius":10 },
+
+                      {
+                      "x_coordinate":10,
+                      "y_coordinate":43,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":5,
+                      "outerRadius":10 },
+
+                      {
+                      "x_coordinate":17,
+                      "y_coordinate":42,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":5,
+                      "outerRadius":10 },
+
+                      {
+                      "x_coordinate":24,
+                      "y_coordinate":41,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":5,
+                      "outerRadius":10 },
+
+                      {
+                      "x_coordinate":27,
+                      "y_coordinate":35,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":5,
+                      "outerRadius":10 },
+
+                      {
+                      "x_coordinate":38,
+                      "y_coordinate":38,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":5,
+                      "outerRadius":10 },
+
+                      {
+                      "x_coordinate":38,
+                      "y_coordinate":46,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":5,
+                      "outerRadius":10 },
+
+                      {
+                      "x_coordinate":54,
+                      "y_coordinate":50,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":5,
+                      "outerRadius":10 },
+
+                      {
+                      "x_coordinate":65,
+                      "y_coordinate":51,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":5,
+                      "outerRadius":10 },
+
+                      {
+                      "x_coordinate":52,
+                      "y_coordinate":44,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":5,
+                      "outerRadius":10 },
+
+                      {
+                      "x_coordinate":59,
+                      "y_coordinate":34,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":5,
+                      "outerRadius":10 },
+
+                      {
+                      "x_coordinate":68,
+                      "y_coordinate":32,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":5,
+                      "outerRadius":10 },
+
+                      {
+                      "x_coordinate":67,
+                      "y_coordinate":30,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":5,
+                      "outerRadius":10 },
+
+                      {
+                      "x_coordinate":27,
+                      "y_coordinate":27,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":5,
+                      "outerRadius":10 },
+
+                      {
+                      "x_coordinate":37,
+                      "y_coordinate":21,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":5,
+                      "outerRadius":10 },
+
+                      {
+                      "x_coordinate":49,
+                      "y_coordinate":17,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":5,
+                      "outerRadius":10 },
+
+                      {
+                      "x_coordinate":33,
+                      "y_coordinate":5,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":5,
+                      "outerRadius":10 },
+
+                      {
+                      "x_coordinate":33,
+                      "y_coordinate":2,
+                      "color":"yellow",
+                      "numPoints":5,
+                      "innerRadius":5,
+                      "outerRadius":10 }
+                      ]
+};
