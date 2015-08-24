ce1949ba720585ce8de4d0d342157fac04852bd9
Felise Uy <feliseuy@ca.ibm.com>
2015-08-24 00:28:56 -0400

    add exercise files

---
+++ exercise_files/stars.js
@@ -0,0 +1,27 @@
+stars = {  
+            "stars":[
+            {
+            "x_coordinate":2,
+            "y_coordinate":4,
+            "color":"yellow",
+            "numPoints":5 },
+
+            {
+            "x_coordinate":6,
+            "y_coordinate":10,
+            "color":"orange",
+            "numPoints": 7 },
+
+            {
+            "x_coordinate":9,
+            "y_coordinate":14,
+            "color":"white",
+            "numPoints":6 }
+
+            //add the rest of your coordinates here. 
+            //Remember to add the quote signs, commas and curly braces! :)
+
+
+            ]
+
+};
