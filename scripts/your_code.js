03703f625a41015f8b2e4c00d0a92b471a7e565f
Hailey Ryu <hailey@ca.ibm.com>
2016-07-18 12:02:20 -0400

    Added a js file for a student to work on.

---
+++ scripts/your_code.js
@@ -0,0 +1,5 @@
+var description = document.getElementById("description");
+var date = new Date().toDateString()
+
+description.innerHTML = "I'm Hailey. Today is " + date + "."
+description.style.color = "pink"
